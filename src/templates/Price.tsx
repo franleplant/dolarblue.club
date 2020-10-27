2d6b87cb782feba6ff478e44bb9f6901d4afd43c
Fran Guijarro <franleplant@gmail.com>
2020-10-27 00:05:28 -0300

    fetch

--- src/templates/Price.tsx
+++ src/templates/Price.tsx
@@ -1,10 +1,22 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { PageProps, Link, graphql } from "gatsby"
 
-import { getPrice } from "../dal/price"
+import { getPrice, IPrice } from "../dal/price"
 
 export default function Price(props: PageProps) {
+  const [price, setPrice] = useState<IPrice | undefined>()
+
+  useEffect(() => {
+    async function fetchData(): Promise<void> {
+      const price = await getPrice()
+      setPrice(price)
+    }
+
+    fetchData()
+  }, [])
+
   console.log("props", props)
+  console.log("astads", price)
 
   return <div>FUCK</div>
 }
