68ad3b805b3ad25a1bb63524cbb4a830dd1025a8
Fran Guijarro <franleplant@gmail.com>
2020-10-26 23:50:19 -0300

    initial commit

---
+++ src/templates/Price.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import { PageProps, Link, graphql } from "gatsby"
+
+export default function Price(props: PageProps) {
+  console.log("props", props)
+
+  return <div>FUCK</div>
+}
