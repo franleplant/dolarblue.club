42619ed6c885efdddd19e5e1e153445acf1d815d
Fran Guijarro <franleplant@gmail.com>
2020-10-28 11:25:39 -0300

    google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,11 @@ module.exports = {
         icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
+
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: { trackingId: "UA-154632381-3" },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
